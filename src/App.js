16e53c60b1bcbf4541c2ff6e7515f8522f8a930c
archana mondal <archanamondal995@gmail.com>
2021-09-23 19:07:26 +0530

    responsiveness implemented

--- src/App.js
+++ src/App.js
@@ -1,119 +1,153 @@
-import React, { useState, useEffect } from 'react'
-import {saveAs} from 'file-saver'
-import axios from 'axios'
-import styled from 'styled-components'
-import './App.css'
-import loader from './imgs/Spinner.gif'
+import React, { useState, useEffect } from "react";
+import { saveAs } from "file-saver";
+import axios from "axios";
+import styled from "styled-components";
+import "./App.css";
+import loader from "./imgs/Spinner.gif";
 
-const Container = styled.div
-`
-  height:100vh; 
-  width:100vw; 
-  display:flex; 
-  align-items:center;
-  justify-content:center; 
-  flex-direction:column;
-  background-color: #00DBDE;
-  background-image: linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%);    
-`
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #00dbde;
+  background-image: linear-gradient(90deg, #00dbde 0%, #fc00ff 100%);
+`;
 
-const NextButton = styled.button
-`
-height:40px; 
-width:auto;
-margin:30px; 
-padding:10px 20px;
-border:1px solid #3498db; 
-font-size:20px; 
-cursor:pointer; 
-font-family:montserrat;
- color:white;
- transition:0.8s; 
- position:relative; 
- overflow:hidden; 
- background-color:#B53471;
+const NextButton = styled.button`
+  height: 40px;
+  width: auto;
+  margin: 30px;
+  padding: 0px 20px;
+  border: 1.5px solid #33d9b2;
+  font-size: 20px;
+  cursor: pointer;
+  font-family: montserrat;
+  color: #fff;
+  transition: 0.8s;
+  position: relative;
+  overflow: hidden;
+  background:none;
+  border-radius: 30px;
 
- &:hover{
-  background-color:#9b59b6;
-  box-shadow: 1px 5px #636e72;
- }
-`
+  &:hover {
+    box-shadow: 0 5px 50px 0 #33d9b2 inset, 0 5px 50px 0 #3c40c6;
+  }
+  @media (min-width:0px) and (max-width:600px){
+    font-size: 18px;
+    padding: 5px 20px;
+    height: auto;
+  }
+`;
 
-const DownloadButton = styled.button
-`
-height:40px; 
-width:auto;
-margin:30px; 
-padding:10px 20px;
-border:1px solid #3498db; 
-font-size:20px; 
-cursor:pointer; 
-font-family:montserrat;
- color:white;
- transition:0.8s; 
- position:relative; 
- overflow:hidden; 
- background-color:#B53471;
+const DownloadButton = styled.button`
+  height: 40px;
+  width: auto;
+  margin: 30px;
+  padding: 0px 20px;
+  border: 1.5px solid #33d9b2;
+  font-size: 20px;
+  cursor: pointer;
+  font-family: montserrat;
+  color: #fff;
+  transition: 0.8s;
+  position: relative;
+  overflow: hidden;
+  background:none;
+  border-radius: 30px;
 
- &:hover{
-  background-color:#9b59b6;
-  box-shadow: 1px 5px #636e72;
- }
-`
+  &:hover {
+    /* background-color: #9b59b6;
+    box-shadow: 1px 5px #636e72; */
+    box-shadow: 0 5px 50px 0 #33d9b2 inset, 0 5px 50px 0 #3c40c6;
+  }
+  @media (min-width:0px) and (max-width:600px){
+    font-size: 18px;
+    padding: 5px 20px;
+    height: auto;
+  }
 
-const Heading = styled.h1
-  `
-  color:#fff;
-  font-family: 'Dancing Script', cursive;
-  font-size:40px;
-  font-weight:600;
-  `
+`;
+
+const Heading = styled.h1`
+  color: #fff;
+  font-family: "Dancing Script", cursive;
+  font-size: 40px;
+  font-weight: 600;
+`;
+
+const Img = styled.img`
+  height: 400px;
+  width: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover{
+    box-shadow: 0 5px 50px 0 #33d9b2 inset, 0 5px 50px 0 #3c40c6;
+  }
+    @media (min-width:0px) and (max-width:600px){
+    width: 300px;
+  }
+`;
+
+
 
 function App() {
   const [imgUrl, setImgUrl] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const getMemes = () =>{
-    axios.get('https://meme-api.herokuapp.com/gimme')
-    .then((response) => {
-      setImgUrl(response.data.url)
-    }).catch((err) =>{
-      console.log(err)
-    })
-  }
+  const getMemes = () => {
+    setLoading(true);
+    axios
+      .get("https://meme-api.herokuapp.com/gimme")
+      .then((response) => {
+        setImgUrl(response.data.url);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  const downloadImage = () =>{
-    saveAs(imgUrl, 'meme.jpg')
-  }
+  const downloadImage = () => {
+    saveAs(imgUrl, "meme.jpg");
+  };
 
   useEffect(() => {
-    getMemes()
-  }, [])
+    getMemes();
+  }, []);
 
-  if(imgUrl.length === 0){
+  if (loading) {
     return (
       <Container>
-        <img src={loader} alt="" style={{height:"50px", width:"50px"}} />
+        <img src={loader} alt="" style={{ height: "50px", width: "50px" }} />
       </Container>
-    )
+    );
   }
 
   return (
     <>
-    <Container>
-    <Heading>Meme Generator</Heading>
+      <Container>
+        <Heading>Meme Generator</Heading>
 
-      <img src={imgUrl} alt="" style={{height:"400px", width:"auto", display:"flex", alignItems:"center", justifyContent:"center"}} />
+        {/* <img src={imgUrl} alt="" style={{height:"400px", width:"auto", display:"flex", alignItems:"center", justifyContent:"center"}} /> */}
+        <Img src = {imgUrl} alt=""/>
 
-    <div style={{display:"flex"}}>
-      <NextButton onClick={getMemes}> Get new meme </NextButton>
-      <DownloadButton onClick={downloadImage}> Download meme </DownloadButton>
-      </div>
-    </Container>
-
+        <div style={{ display: "flex" }}>
+          <NextButton onClick={getMemes}> Get New </NextButton>
+          <DownloadButton onClick={downloadImage}>
+            {" "}
+            Download{" "}
+          </DownloadButton>
+        </div>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
 //Download button with functionality
